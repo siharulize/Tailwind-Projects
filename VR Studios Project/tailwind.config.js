e92431f8414a8e5ed2a75f1b4a5c79c2b3f06ca1
siharulize <broouwnie@gmail.com>
2024-06-25 13:01:38 +0200

    VR Studio Project

---
+++ VR Studios Project/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1140px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      }
+    },
+  },
+  plugins: [],
+}
+
